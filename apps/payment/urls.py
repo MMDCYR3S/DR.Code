649470ee6c4c61a1610fc3360522de2cedd298d3
CYR3S <amingholami06@gmail.com>
2025-10-16 17:28:36 +0330

    ایجاد صفحه بعد از پرداخت

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import RequestView
+from .views import RequestView, AfterPayView
 
 urlpatterns = [
-    path("request/", RequestView.as_view(), name="payment-request")
+    path("request/", RequestView.as_view(), name="payment-request"),
+    path("status/", RequestView.as_view(), name="payment-status"),
 ]
 
--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
 
-# ======= AFTER PAY VIEW ======= #
+# ======= REQUEST VIEW ======= #
 class RequestView(TemplateView):
     """ نمایش وضعیت پرداخت بعد از درگاه پرداخت """
+    
     template_name = "payment/request.html"
+# ======= AFTER PAY VIEW ======= #
+class AfterPayView(TemplateView):
+    """ نمایش وضعیت پرداخت بعد از درگاه پرداخت """
+    template_name = "payment/after_pay.html"
