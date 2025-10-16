fa354715c39020da6e1dddf069534d45e407f65c
Mahdi Rahimi <AbstractAxisX@gmail.com>
2025-10-16 16:17:55 +0330

    حاوی ادیت های ریز

--- static/js/prescription_detail.js
+++ static/js/prescription_detail.js
@@ -646,7 +646,7 @@ function createProtectedWatermark() {
             width: 100%;
             height: 100%;
             pointer-events: none;
-            z-index: 999999;
+            z-index:1;
             overflow: hidden;
         `;
         
